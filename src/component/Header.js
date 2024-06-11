f632da2636f370942723abcdf08647c8d09428c6
Jagmeet <jagmeet2001singh@gmail.com>
2024-06-11 23:38:38 +0530

    correct home link name

--- src/component/Header.js
+++ src/component/Header.js
@@ -42,7 +42,7 @@ const Header = () => {
             <div className="flex-div gap-[2rem]">
               <li className="nav-item">
                 <NavLink to="/" className="nav-links" onClick={toggleMenu}>
-                  Home123
+                  Home
                 </NavLink>
               </li>
               <li className="nav-item">
